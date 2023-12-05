191fa37f1b9336e8fed3e716d5fab24badf4fad9
gabrieltassinari <100461539+gabrieltassinari@users.noreply.github.com>
2023-12-05 18:08:39 -0300

    Update: composer script

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ composer() {
 
 	mkdir -p $LOCAL
 
-	sudo sed -Ei 's/;extension=pdo_pgsql/extension=pdo_pgsql/g;s/;extension==pgsql/extension=pgsql/g' /etc/php/8.1/apache2/php.ini /etc/php/8.1/cli/php.ini
-
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 	php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 	php composer-setup.php
@@ -16,6 +14,8 @@ composer() {
 	mv composer.phar $LOCAL
 
 	echo "export PATH=\$PATH:$LOCALPHP" >> $HOME/.bashrc
+
+	sudo sed -Ei 's/;extension=pdo_pgsql/extension=pdo_pgsql/g;s/;extension=pgsql/extension=pgsql/g' /etc/php/8.1/apache2/php.ini /etc/php/8.1/cli/php.ini
 }
 
 composer
